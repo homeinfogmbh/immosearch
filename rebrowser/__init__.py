a27564465df9f1676557fb7d4ffadc3dfd2d5121
Richard Neumann <r.neumann@homeinfo.de>
2014-10-21 17:44:05 +0200

    Added metadata

--- rebrowser/__init__.py
+++ rebrowser/__init__.py
@@ -3,3 +3,5 @@ An OpenImmo™ and style sheet based real estate browser
 """
 __author__ = 'Richard Neumann <r.neumann@homeinfo.de>'
 __date__ = '21.10.2014'
+
+from .wsgi_interpreter import WSGIEnvInterpreter
---
+++ setup.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+	name='rebrowser',
+	version='0.0.1',
+	author='Richard Neumann',
+	author_email='mail@richard-neumann.de',
+    install_requires=['openimmo'],
+	packages=['rebrowser'],
+	license=open('LICENSE.txt').read(),
+	description='Real estate browser',
+	long_description=open('README.txt').read(),
+)
