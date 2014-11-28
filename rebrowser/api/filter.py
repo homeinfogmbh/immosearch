23e60d76cb3f51267ba12d8812a7f323436b4eb3
Richard Neumann <mail@richard-neumann.de>
2014-11-28 17:44:19 +0100

    fix

--- rebrowser/api/filter.py
+++ rebrowser/api/filter.py
@@ -1,65 +1,100 @@
 """
-Real estate filter
+Real estate
 """
 __author__ = 'Richard Neumann <r.neumann@homeinfo.de>'
 __date__ = '28.11.2014'
-__all__ = ['Filter']
+__all__ = ['FilterableRealEstate']
 
 
-class Filter():
+# TODO: properties:
+"""
+Objektart
+
+Land
+Stadt
+Ort
+PLZ
+Straße
+Hausnummer
+
+Zimmer
+Etage
+Wohnfläche
+Grundstücksfläche
+Balkon
+Terrasse
+
+Kaltmiete
+Warmmiete
+Nebenkosten
+Kaufpreis
+"""
+
+
+class FilterableRealEstate():
     """
-    A real estate filter
+    Wrapper class for an OpenImmo™-Immobilie
+    that can be filtered by certain attributes
     """
-    def __init__(self, fdict=None):
-        """Initializes with a filter dictionary"""
-        self.__fdict = {} if fdict is None else fdict
+    def __init__(self, immobilie):
+        """Sets the appropriate OpenImmo™-immobilie"""
+        self.__immobilie = immobilie
 
     @property
-    def fdict(self):
-        """Returns the filter dictionary"""
-        return self.__fdict
+    def immobilie(self):
+        """Returns the appropriate immobilie"""
+        return self.__immobilie
 
-    def add_rule(self, node, ):
-        pass
-
-
-class Rule():
-    """
-    A filter rule
-    """
-    def __init__(self, node, op, values):
-        """Initializes a filter rule"""
-        self.__node = node
-        self.__op = op
-        self.__values = values
+    @immobilie.setter
+    def _immobilie(self, immobilie):
+        """Sets the appropriate immobilie"""
+        self.__immobilie = immobilie
 
     @property
-    def node(self):
-        """Returns the filtering node"""
-        return self.__node
-
-    @property
-    def op(self):
-        """Returns the desired operation"""
-        return self.__op
-
-    @property
-    def values(self):
-        """Returns the filter values"""
-        return self.__values
-
-    def _node_data(self, real_estate):
-        """Returns the data of the targeted node"""
-        if self.node == 'openimmo_obid':
-            return str(real_estate.verwaltung_techn.openimmo_obid)
-        elif self.node == 'wohnflaeche':
-            # TODO: implement further
-            pass
-
-    def apply(self, real_estates):
-        """Applies the rule on an iterable real_estates"""
-        for real_estate in real_estates:
-            if self._filter(real_estate):
-                yield real_estate
-        node_data = self._node_data()
-        # TODO: implement further
+    def types(self):
+        """Returns a generator of the object's types"""
+        oa = self.immobilie.objektkategorie.objektart
+        for zimmer in oa.zimmer:
+            yield str(zimmer.zimmertyp) if zimmer.zimmertyp else 'ZIMMER'
+        for wohnung in oa.wohnung:
+            yield str(wohnung.wohnungtyp) if wohnung.wohnungtyp else 'WOHNUNG'
+        for haus in oa.haus:
+            yield str(haus.haustyp) if haus.haustyp else 'HAUS'
+        for grundstueck in oa.grundstueck:
+            yield (str(grundstueck.grundst_typ)
+                   if grundstueck.grundst_typ
+                   else 'GRUNDSTUECK')
+        for buero_praxen in oa.buero_praxen:
+            yield (str(buero_praxen.buero_typ)
+                   if buero_praxen.buero_typ
+                   else 'BUERO_PRAXEN')
+        for einzelhandel in oa.einzelhandel:
+            yield (str(einzelhandel.handel_typ)
+                   if einzelhandel.handel_typ
+                   else 'EINZELHANDEL')
+        for gastgewerbe in oa.gastgewerbe:
+            yield (str(gastgewerbe.gastgew_typ)
+                   if gastgewerbe.gastgew_typ
+                   else 'GASTGEWERBE')
+        for hallen_lager_prod in oa.hallen_lager_prod:
+            yield (str(hallen_lager_prod.hallen_typ)
+                   if hallen_lager_prod.hallen_typ
+                   else 'HALLEN_LAGER_PROD')
+        for land_und_forstwirtschaft in oa.land_und_forstwirtschaft:
+            yield (str(land_und_forstwirtschaft.land_typ)
+                   if land_und_forstwirtschaft.land_typ
+                   else 'LAND_UND_FORSTWIRTSCHAFT')
+        for parken in oa.parken:
+            yield str(parken.parken_typ) if parken.parken_typ else 'PARKEN'
+        for sonstige in oa.sonstige:
+            yield (str(sonstige.sonstige_typ)
+                   if sonstige.sonstige_typ
+                   else 'SONSTIGE')
+        for freizeitimmobilie_gewerblich in oa.freizeitimmobilie_gewerblich:
+            yield (str(freizeitimmobilie_gewerblich.freizeit_typ)
+                   if freizeitimmobilie_gewerblich.freizeit_typ
+                   else 'FREIZEITIMMOBILIE_GEWERBLICH')
+        for zinshaus_renditeobjekt in oa.zinshaus_renditeobjekt:
+            yield (str(zinshaus_renditeobjekt.zins_typ)
+                   if zinshaus_renditeobjekt.zins_typ
+                   else 'ZINSHAUS_RENDITEOBJEKT')
