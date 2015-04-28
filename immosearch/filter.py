2642f94286be497d36bda1db965242403a0b5d3e
Richard Neumann <r.neumann@homeinfo.de>
2015-04-28 16:55:31 +0200

    debug

--- immosearch/filter.py
+++ immosearch/filter.py
@@ -187,6 +187,8 @@ class RealEstateSieve():
                             option_format = None
                             option_func = option_
                         value = cast(raw_value, typ=option_format)
+                        raise FilterOperationNotImplemented(
+                            ' - '.join([option_func(real_estate), value]))
                         try:
                             result = operation_func(option_func(real_estate),
                                                     value)
