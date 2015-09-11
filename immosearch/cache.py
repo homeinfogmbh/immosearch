d725bfa72aec329d59ebabc7c72ce06c0e9937fe
Richard Neumann <r.neumann@homeinfo.de>
2015-09-11 16:44:44 +0200

    debug

--- immosearch/cache.py
+++ immosearch/cache.py
@@ -19,6 +19,7 @@ class CacheManager():
         """Iterates over the user's real estates"""
         cid = self._user.cid
         if self._cache is None:
+            raise Exception('DEBUG1')
             for i in Immobilie.by_cid(cid):
                 yield i.immobilie
         else:
@@ -26,10 +27,12 @@ class CacheManager():
             try:
                 cached_data = self._cache[cid]
             except KeyError:
+                raise Exception('DEBUG2')
                 real_estates = [i.immobilie for i in Immobilie.by_cid(cid)]
                 self._cache[cid] = (real_estates, now)
                 yield from real_estates
             else:
+                raise Exception('DEBUG3')
                 real_estates, cache_time = cached_data
                 if now - cache_time >= timedelta(seconds=self._refresh):
                     real_estates = [i.immobilie for i in Immobilie.by_cid(cid)]
