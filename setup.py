35e876084db69672d97dd8983c6263c369148aa4
Richard Neumann <r.neumann@homeinfo.de>
2018-04-11 11:02:28 +0200

    Removed obsolete files.

--- setup.py
+++ setup.py
@@ -7,7 +7,5 @@ setup(
     version='latest',
     author='Richard Neumann',
     packages=['immosearch'],
-    scripts=['files/immosearchd'],
-    data_files=[('/usr/lib/systemd/system', ['files/immosearch.service'])],
     license=open('LICENSE.txt').read(),
     description='Real estate search engine.')
