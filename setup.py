7d5f72e8f776e36cdc3f8d71a8cf3005b4c0db7c
Richard Neumann <mail@richard-neumann.de>
2021-01-26 16:39:55 +0100

    Migrated to setuptools.

--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
 #! /usr/bin/env python3
+"""Install script."""
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='immosearch',
-    version='latest',
-    author='Richard Neumann',
+    name='mdb',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'boolparse',
+        'configlib',
+        'flask',
+        'mdb',
+        'openimmo',
+        'openimmodb',
+        'openimmolib',
+        'peewee',
+        'peeweeplus',
+        'pyxb',
+        'wsgilib'
+    ],
+    author='HOMEINFO - Digitale Informationssysteme GmbH',
+    author_email='info@homeinfo.de',
+    maintainer='Richard Neumann',
+    maintainer_email='r.neumann@homeinfo.de',
     packages=['immosearch'],
     license=open('LICENSE.txt').read(),
-    description='Real estate search engine.')
+    description='Real estate search engine.'
+)
