c65298a465c4dafa0a28b641b7f2dc001de4a572
Richard Neumann <r.neumann@homeinfo.de>
2022-01-03 15:02:50 +0100

    Add type hints

--- immosearch/pager.py
+++ immosearch/pager.py
@@ -1,5 +1,7 @@
 """Element paging"""
 
+from typing import Any, Iterable, Optional
+
 
 __all__ = ['Pager']
 
@@ -7,20 +9,22 @@ __all__ = ['Pager']
 class Pager:    # pylint: disable=R0903
     """Browses through pages"""
 
-    def __init__(self, items, limit=None, page=None):
+    def __init__(self, items: Iterable[Any], limit: Optional[int] = None,
+                 page: Optional[int] = None):
         """Sets the respective items and the page size limit"""
         self.items = items
         self.limit = limit
         self.page = page or 0
 
-    def __iter__(self):
+    def __iter__(self) -> Any:
         """Yields items of nth page"""
         if self.limit is None:
             yield from self.items
-        else:
-            start = self.page * self.limit
-            end = (self.page + 1) * self.limit - 1
+            return
 
-            for i, item in enumerate(self.items):
-                if start <= i <= end:
-                    yield item
+        start = self.page * self.limit
+        end = (self.page + 1) * self.limit - 1
+
+        for i, item in enumerate(self.items):
+            if start <= i <= end:
+                yield item
